519810dec3c295710f8326b7cecdd46a5e4dc014
juanrtato <juanrtato@gmail.com>
2025-05-17 17:19:43 -0500

    :zap: mappings and some util functions were added

--- scripts/utils.py
+++ scripts/utils.py
@@ -7,6 +7,8 @@ import json
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 
+from mappings import *
+
 
 METADATA_PATH = '../datalake/PASTIS24/metadata.geojson'
 with open(METADATA_PATH, 'r') as f:
@@ -187,3 +189,75 @@ def plot_mean_band_profile(inputs, sample_idx):
     plt.tight_layout()
     plt.show()
 
+
+def mask_to_text(mask_array: np.ndarray, label_names: dict, language: str = "en") -> str:
+    """
+    Convert a segmentation mask to a text representation.
+    
+    Args:
+        mask_array (np.ndarray): Segmentation mask with class labels.
+        label_names (dict): Dictionary mapping class indices to labels.
+        language (str): Language for the output text ("es" for Spanish, "en" for English).
+    
+    Returns:
+        str: Text representation of the segmentation mask.
+    """
+    if language == "es":
+        initial_msg = INITIAL_MSGS_ES
+        end_msg = END_MSGS_ES
+        no_crop_msg = NO_CROP_MSGS_ES
+        ext_msg = EXTENTION_MSGS_ES
+    elif language == "en":
+        initial_msg = INITIAL_MSGS_EN
+        end_msg = END_MSGS_EN
+        no_crop_msg = NO_CROP_MSGS_EN
+        ext_msg = EXTENTION_MSGS_EN
+    else:
+        raise ValueError("Language not supported. Use 'es' for Spanish or 'en' for English.")
+        
+    initial_msg = initial_msg[np.random.randint(0, len(initial_msg))]
+    area_per_class = get_area_per_class(mask_array, label_names)
+    class_names = list(area_per_class.keys())
+    if len(class_names) == 0 or all(
+        class_name in ["Etiqueta vacía", "Fondo", "Background", "Void label"]
+        for class_name in class_names
+    ):
+        return no_crop_msg[np.random.randint(0, len(no_crop_msg))]
+
+    total_area = sum(area_per_class.values())
+    for class_name, area in area_per_class.items():
+        if class_name not in ["Etiqueta vacía", "Fondo", "Background", "Void label"]:
+            initial_msg += f"{class_name.lower()} {ext_msg[np.random.randint(0, len(ext_msg))]} {area} m2, "
+        #else:
+        # TODO: que hacer con el fondo y etiqueta vacia?
+
+    initial_msg += f"{end_msg[np.random.randint(0, len(end_msg))]}{total_area} m2."
+    text_representation = initial_msg + "\n"
+
+    return text_representation
+
+
+def get_area_per_class(seg_mask: np.array, label_names: dict, area_px_m2: int = 100) -> dict:
+    """
+    Get the area (in m2) of each class in the segmentation mask.
+    Args:
+        seg_mask (np.ndarray): Segmentation mask with class labels.
+        classes (dict): Dictionary mapping class labels to their respective names.
+        area_px_m2 (float): Area of one pixel in square meters.
+        label_names (dict): Dictionary mapping class indices to labels.
+    
+    Returns:
+        dict: Dictionary with class labels as keys and their respective areas in square meters.
+    """
+    classes, counts = np.unique(seg_mask, return_counts=True)
+    if label_names is not None:
+        areas_per_class = {
+            label_names.get(str(int(crop_class))): int(count * area_px_m2)
+            for crop_class, count in zip(classes, counts)
+        }
+        return areas_per_class
+
+    return {
+            str(int(crop_class)): int(count * area_px_m2)
+            for crop_class, count in zip(classes, counts)
+        }
